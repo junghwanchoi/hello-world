769a472524217a8fa30b53bdfb7acb1792ccb89a
JungHwan <59270365+junghwanchoi@users.noreply.github.com>
2022-09-21 03:49:00 +0900

    긴 Path 이름 해결방법

---
+++ 긴 Path 해결방법.py
@@ -0,0 +1,16 @@
+
+# (출처) https://www.daniweb.com/programming/software-development/threads/124812/long-paths-file-names-ext-s
+
+# You probably syntaxed the the file incorrectly :P
+# Copy and paste the full directory name, replacing my string. Then simply add a forward slash to escape the ones already there!
+
+import os
+
+filepath = "C:\\Documents and Settings\\Username\\Desktop\\coolapps\\pythonprograms\\zomglongfilename.file"
+
+info = os.path.split(filepath)
+
+os.chdir(info[0])
+
+print os.path.getsize(info[-1])
+
